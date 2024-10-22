576cc2ca5eff7f974e17ecbb1d06033d042f5407
meirk_wlhs <meirk@wlhs.wlwv.k12.or.us>
2024-10-22 09:55:15 -0700

    Finished

--- lesson1.py
+++ lesson1.py
@@ -5,3 +5,34 @@ Description: Unit 4 Lesson 1 - while loops
 
 '''
 
+# While loops run while a condition is true
+# we usually don't know how many times, this
+# scenario is when they are most often used
+
+'''
+
+some_condition
+
+while some_condition is true:
+    code to execute
+    update variable (if forgotten -> infinite loop)
+'''
+
+start_number = 10
+while start_number > 0:
+    print(start_number)
+    start_number = start_number - 1
+
+if start_number == 0:
+    print("0")
+    print("BLASTOFF!!")
+# Change the code so it prints 10 9 ... 0 Blastoff!
+
+# making a loop run to get user input
+
+while True:
+    user_age = int(input("Enter age or -1 to quit: "))
+    if user_age == -1:
+        break # exit the loop
+    else:
+        continue # return to top of loop
